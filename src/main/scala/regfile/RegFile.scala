69f9050b63c9a82aca031af30c78cbb682aa78ba
liuyic00 <liuyic00@gmail.com>
2021-12-17 10:56:09 +0800

    refactor: minimize bug

--- src/main/scala/regfile/RegFile.scala
+++ src/main/scala/regfile/RegFile.scala
@@ -2,54 +2,28 @@ package regfile
 
 import chisel3._
 
-class WriteBack extends Bundle {
+class WriteIO extends Bundle {
   val valid = Input(Bool())
-  val dest  = Input(UInt(8.W))
+  val dest  = Input(UInt(2.W))
   val data  = Input(UInt(64.W))
 }
 
-class State extends Bundle {
-  val reg = Output(Vec(32, UInt(64.W)))
+class ReadIO extends Bundle {
+  val idx  = Input(UInt(2.W))
+  val data = Output(UInt(64.W))
 }
 
 class RegFile extends Module {
   val io = IO(new Bundle {
-    val wb    = Input(new WriteBack)
-    val state = Output(new State)
-    val rd = new Bundle {
-      val idx  = Input(UInt(8.W))
-      val data = Output(UInt(64.W))
-    }
+    val wt = new WriteIO
+    val rd = new ReadIO
   })
 
-  val reg = RegInit(VecInit(Seq.fill(32)(0.U(64.W))))
+  val reg = RegInit(VecInit(Seq.fill(4)(0.U(64.W))))
 
-  when(io.wb.valid) {
-    reg(io.wb.dest) := io.wb.data
+  when(io.wt.valid) {
+    reg(io.wt.dest) := io.wt.data
   }
-
-  io.state.reg := reg
 
   io.rd.data := reg(io.rd.idx)
-}
-
-class RegFileChecker extends Module {
-  val io = IO(new Bundle {
-    val wb    = Input(new WriteBack)
-    val state = Input(new State)
-    val rd = new Bundle {
-      val idx  = Input(UInt(8.W))
-      val data = Input(UInt(64.W))
-    }
-  })
-
-  val regFile = Module(new RegFile)
-
-  regFile.io.wb     := io.wb
-  regFile.io.rd.idx := io.rd.idx
-
-  for (i <- 0 until 32) {
-    assert(regFile.io.state.reg(i.U) === io.state.reg(i.U))
-  }
-  assert(regFile.io.rd.data === io.rd.data)
 }
--- src/test/scala/regfile/RegFileSpec.scala
+++ src/test/scala/regfile/RegFileSpec.scala
@@ -6,48 +6,56 @@ import chiseltest.formal._
 import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
 
-class Checker extends Module {
+class CheckerA extends Module {
   val io = IO(new Bundle {
-    val wb    = Input(new WriteBack)
-    val state = Input(new State)
-    val rd = new Bundle {
-      val idx  = Input(UInt(8.W))
-      val data = Output(UInt(64.W))
-    }
+    val wt = new WriteIO
+    val rd = new ReadIO
   })
 
   val regFile1 = Module(new RegFile)
   val regFile2 = Module(new RegFile)
 
-  regFile1.io.wb := io.wb
-  regFile2.io.wb := io.wb
+  regFile1.io.wt := io.wt
+  regFile2.io.wt := io.wt
 
   regFile1.io.rd.idx := io.rd.idx
   regFile2.io.rd.idx := io.rd.idx
 
-  for (i <- 0 until 32) {
-    assert(regFile1.io.state.reg(i.U) === regFile2.io.state.reg(i.U))
-  }
-  assert(regFile1.io.rd.data === regFile2.io.rd.data)
+  // here is a bug on purpos，
+  assert(regFile1.io.rd.data =/= regFile2.io.rd.data)
+  // assert(regFile1.io.rd.data === regFile2.io.rd.data)
 
   io.rd.data := regFile1.io.rd.data
 }
 
-class RegFileSpec extends AnyFlatSpec with Formal with ChiselScalatestTester {
-  behavior of "RegFile"
-  it should "pass" in {
-    verify(new Checker, Seq(BoundedCheck(5)))
+class CheckerB extends Module {
+  val io = IO(new Bundle {
+    val wt = new WriteIO
+    val rd = new ReadIO
+  })
+
+  val reg1 = RegInit(VecInit(Seq.fill(4)(0.U(64.W))))
+  val reg2 = RegInit(VecInit(Seq.fill(4)(0.U(64.W))))
+
+  when(io.wt.valid) {
+    reg1(io.wt.dest) := io.wt.data
+    reg2(io.wt.dest) := io.wt.data
   }
 
-  behavior of "RegFileChecker"
+  // here is a bug on purpos，
+  assert(reg1(io.rd.idx) =/= reg2(io.rd.idx))
+  // assert(reg1(io.rd.idx) === reg2(io.rd.idx))
+
+  io.rd.data := reg1(io.rd.idx)
+}
+
+class RegFileSpec extends AnyFlatSpec with Formal with ChiselScalatestTester {
+  behavior of "CheckerA"
   it should "pass" in {
-    class TestModule extends RegFile {
-      val checker = Module(new RegFileChecker)
-      checker.io.wb        := io.wb
-      checker.io.state.reg := reg
-      checker.io.rd.idx    := io.rd.idx
-      checker.io.rd.data   := io.rd.data
-    }
-    verify(new TestModule, Seq(BoundedCheck(5)))
+    verify(new CheckerA, Seq(BoundedCheck(5)))
+  }
+  behavior of "CheckerB"
+  it should "pass" in {
+    verify(new CheckerB, Seq(BoundedCheck(5)))
   }
 }
